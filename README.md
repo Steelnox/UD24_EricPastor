8df30c44ddcbc295c8c9c723f71ef757e9e0eacc
Steelnox <eric14pastor@gmail.com>
2021-02-02 12:10:12 +0100

    Ejercicio 1 hecho

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # UD24_EricPastor
+
+Ejercicio 1 (Capturas de pantalla)
+POST
+http://prntscr.com/y088iy
+PUT
+https://prnt.sc/y09coq
+GET
+https://prnt.sc/y09rq1
+DELETE
+https://prnt.sc/y0aszg
+SWAGGER
+https://prnt.sc/y0d64j
+
+
+
+
+
---
+++ UD22_Ejercicio1/UD22_Ejercicio1/Models/APIContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace UD22_Ejercicio1.Models
+{
+    public class APIContext : DbContext
+    {
+        public APIContext(DbContextOptions<APIContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+        }
+    }
+}
